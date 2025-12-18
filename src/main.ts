7794369db11a9acf1a88cade6a05a6442db68101
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2025-12-18 18:00:15 +0200

    feat: implement service to service auth

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AiCoreInternalAuthGuard } from './ai-core-internal-auth/ai-core-internal-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalGuards(new AiCoreInternalAuthGuard());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
