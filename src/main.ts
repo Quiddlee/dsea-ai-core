5a678fb03e9cd5e8f8583fe78cd1b209e360de82
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2025-12-22 17:39:37 +0200

    refactor: move internal auth guard to common

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { AiCoreInternalAuthGuard } from './ai-core-internal-auth/ai-core-internal-auth.guard';
+import { AiCoreInternalAuthGuard } from './common/guards/ai-core-internal-auth/ai-core-internal-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
