f5f04b031386b81908316e529383ceb5cdf085ed
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2026-01-07 12:07:31 +0200

    fix: global auth guard registration

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { AiCoreInternalAuthGuard } from './common/guards/ai-core-internal-auth/ai-core-internal-auth.guard';
 import { AppConfig } from './common/types/environment';
 import { appConfiguration } from './common/config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<AppConfig>(appConfiguration.KEY);
-  const aiCoreInternalAuthGuard = app.get(AiCoreInternalAuthGuard);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalGuards(aiCoreInternalAuthGuard);
 
   await app.listen(config.port);
 }
